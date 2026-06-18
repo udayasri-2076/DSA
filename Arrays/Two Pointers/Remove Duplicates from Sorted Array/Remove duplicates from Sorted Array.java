71d6b66ce61796ede433041af1779d689bb71702
Udayasri <udayas1220@gmail.com>
2026-06-19 00:55:45 +0530

    Add Java program to remove duplicates from sorted array
    Implemented a Java program to remove duplicates from a sorted array and count unique elements.

--- Arrays/Two Pointers/Remove Duplicates from Sorted Array/Remove duplicates from Sorted Array.java
+++ Arrays/Two Pointers/Remove Duplicates from Sorted Array/Remove duplicates from Sorted Array.java
@@ -1,1 +1,24 @@
-
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int arr[]=new int[n];
+		
+		for(int i=0;i<n;i++){
+		    arr[i]=in.nextInt();  //1 1 2 2 2 3 3
+		}
+		
+		int i=0;                //i=0         
+		for(int j=1;j<n;j++){  //j=1 1<7t j=2    j=3    j=4    j=5    j=6
+		    if(arr[i]!=arr[j]){ //1!=1f  1!=2t  2!=2f   2!=2f  3!=2t  3!=3f
+		        arr[i+1]=arr[j]; //a[1]=2                     a[2]=3
+		        i++;            //i=0+1=1                     i=1+1=2
+		    }
+		}
+		
+		System.out.println(i+1);  //3 unique elements (2+1)=3
+	
+	}
+}
