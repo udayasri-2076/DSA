69f805aa5de37381230ce701db72fb025548a9e3
Udayasri <udayas1220@gmail.com>
2026-06-19 01:21:41 +0530

    Implement missing number finder in Java

--- Arrays/Basic Traversal/Find Missing Number/Find Missing Number.java
+++ Arrays/Basic Traversal/Find Missing Number/Find Missing Number.java
@@ -1,1 +1,26 @@
-
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int arr[]=new int[n];  //5
+		
+		for(int i=0;i<n;i++){
+		    arr[i]=in.nextInt();  //1 2 4 5 6
+		}
+		
+		int sum1=(n*(n+1))/2;   //(5*6)/2=15
+		int sum2=0;             //sum2=0
+		for(int i=0;i<n;i++){
+		     
+		    sum2=sum2+arr[i];   //sum2=0+1=1+2=3+4=7+5+6=18
+		    
+		}
+		
+		int miss=sum2-sum1;   //18-15=3
+		
+		System.out.println(miss); //3
+	
+	}
+}
