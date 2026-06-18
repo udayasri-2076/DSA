74e31e7164af4568a6dc4e0ca4031705970b20ea
Udayasri <udayas1220@gmail.com>
2026-06-19 00:38:50 +0530

    Implement array sorted check in Java
    Added a program to check if an array is sorted in non-decreasing order.

--- Arrays/Basic Traversal/Check if Array is Sorted/Check if Array is Sorted.java
+++ Arrays/Basic Traversal/Check if Array is Sorted/Check if Array is Sorted.java
@@ -1,1 +1,27 @@
-
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int arr[]=new int[n];
+		
+		for(int i=0;i<n;i++){
+		    arr[i]=in.nextInt();  //1 2 4 7 7 5
+		}
+		
+		boolean sorted=true;
+		for(int i=1;i<n;i++){   //i=1 1<6t 2<6t  3<6t   4<6t     5<6t
+		    if(arr[i]>=arr[i-1]){//2>1t    4>2t   7>4t   7>=7t   5>7f
+		        sorted=true;   //true    true    true
+		    }
+		    else{
+		        sorted=false;  //false
+		    }
+		}
+		
+		System.out.println(sorted); //false
+		
+	
+	}
+}
