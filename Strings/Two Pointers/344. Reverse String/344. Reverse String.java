b17ea2c62b1197f56a404dbcf7b72bf98dad546c
Udayasri <udayas1220@gmail.com>
2026-07-21 21:58:47 +0530

    Update 344. Reverse String.java

--- Strings/Two Pointers/344. Reverse String/344. Reverse String.java
+++ Strings/Two Pointers/344. Reverse String/344. Reverse String.java
@@ -1,1 +1,39 @@
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();       //5
+        char s[] = new char[n];     //h e l l o
+
+        for(int i = 0; i < n; i++) {
+            s[i] = in.next().charAt(0);   //h e l l o
+        }
+
+        int l = 0;                  //l=0
+        int r = s.length - 1;       //r=4
+
+        while(l < r) {              //l=0 r=4   l=1 r=3   l=2 r=2(stop)
+            char temp = s[l];       //temp=h        temp=e
+            s[l] = s[r];            //s[0]=o        s[1]=l
+            s[r] = temp;            //s[4]=h        s[3]=e
+            l++;                    //l=1           l=2
+            r--;                    //r=3           r=2
+        }
+        //result: o l l e h
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(s[i] + " ");   //o l l e h
+        }
+    }
+}
+
+/*
+Input:
+5
+h e l l o
+
+Output:
+o l l e h
+*/
