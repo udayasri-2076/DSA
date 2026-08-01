c4fb3959d59b4e667c24840e6e59beeb24faca3a
Udayasri <udayas1220@gmail.com>
2026-08-01 22:49:54 +0530

    Add pattern printing program in Pattern 5.java
    This program prints a right-aligned triangle pattern of asterisks based on user input.

---
+++ Logic_Building/Pattern5/Pattern 5.java
@@ -0,0 +1,50 @@
+/* Output
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+*/
+
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();                  // n = 5
+
+        for(int i = 1; i <= n; i++) {          // i=1  i=2  i=3  i=4  i=5
+
+            for(int k = 1; k <= n-i; k++) {    // i=1 -> 4 spaces
+                                               // i=2 -> 3 spaces
+                                               // i=3 -> 2 spaces
+                                               // i=4 -> 1 space
+                                               // i=5 -> 0 spaces
+
+                System.out.print(" ");         // "    "
+                                               // "   "
+                                               // "  "
+                                               // " "
+                                               // ""
+            }
+
+            for(int j = 1; j <= i; j++) {      // i=1 -> j=1
+                                               // i=2 -> j=1,2
+                                               // i=3 -> j=1,2,3
+                                               // i=4 -> j=1,2,3,4
+                                               // i=5 -> j=1,2,3,4,5
+
+                System.out.print("* ");        // *
+                                               // * *
+                                               // * * *
+                                               // * * * *
+                                               // * * * * *
+            }
+
+            System.out.println();
+        }
+    }
+}
