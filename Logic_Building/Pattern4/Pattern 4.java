066cb95adfe7960b23ea8b1a15451b2d242bbbb9
Udayasri <udayas1220@gmail.com>
2026-08-01 21:04:15 +0530

    Add pattern printing logic in Pattern 4.java
    Implement a pattern printing program that outputs numbers in decreasing rows.

---
+++ Logic_Building/Pattern4/Pattern 4.java
@@ -0,0 +1,37 @@
+/* Output
+1 1 1 1 1
+2 2 2 2
+3 3 3
+4 4
+5
+*/
+
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();               // n = 5
+
+        for(int i = 1; i <= n; i++) {       // i=1  i=2  i=3  i=4  i=5
+
+            for(int j = i; j <= n; j++) {   // i=1 -> j=1,2,3,4,5
+                                            // i=2 -> j=2,3,4,5
+                                            // i=3 -> j=3,4,5
+                                            // i=4 -> j=4,5
+                                            // i=5 -> j=5
+
+                System.out.print(i + " ");  // 1 1 1 1 1
+                                            // 2 2 2 2
+                                            // 3 3 3
+                                            // 4 4
+                                            // 5
+            }
+
+            System.out.println();
+        }
+    }
+}
